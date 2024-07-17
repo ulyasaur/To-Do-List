4d51d6c1af39c098759b2ba49414d8283ee730bb
ulyasaur <ulyano4ka2003@gmail.com>
2024-07-18 00:46:45 +0300

    add dtos

---
+++ ToDoList/ToDoList.BLL/Dtos/TaskItemDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.BLL.Dtos.Enums;
+
+namespace ToDoList.BLL.Dtos
+{
+    public class TaskItemDto
+    {
+        public int TaskItemId { get; set; }
+
+        public string Name { get; set; }
+
+        public TaskStatusDto Status { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
