85c6b9babc080f141644ba8db2bb15aff7b23749
ulyasaur <ulyano4ka2003@gmail.com>
2024-07-17 23:04:20 +0300

    add models and enums

---
+++ ToDoList/ToDoList.DAL/Models/Enums/TaskStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.DAL.Models.Enums
+{
+    public enum TaskStatus
+    {
+        ToDo,
+        InProgress,
+        Done
+    }
+}
---
+++ ToDoList/ToDoList.DAL/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.DAL.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
